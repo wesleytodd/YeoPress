72a2b4cdc5c1e56054af87d486e48daaa06c3ac6
Wes Todd <wes@wesleytodd.com>
2013-03-02 00:05:44 -0600

    Fixed sass on load

--- yeopress-init.js
+++ yeopress-init.js
@@ -1,6 +1,6 @@
 var exec = require('child_process').exec;
 
-exec('npm install && jam install', function(err) {
+exec('npm install && jam install && sass scss/global.scss css/global.css', function(err) {
 	if (err) {
 		console.error(err);
 	}
