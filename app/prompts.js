cf7ac337dc2ffbe599fdf3e07db030e0958746aa
Wes Todd <wes@wesleytodd.com>
2013-11-09 22:39:24 -0600

    A ton of changes, should probably have been more than one commit

--- app/prompts.js
+++ app/prompts.js
@@ -1,10 +1,24 @@
-module.exports = function(advanced) {
+module.exports = function(advanced, defaults) {
+
+	// Validate required
+	var requiredValidate = function(value) {
+		if (value == '') {
+			return 'This field is required.';
+		}
+		return true;
+	};
+	// When advanced
+	var advancedWhen = function() {
+		return advanced;
+	};
+
 	return [
 		{
-			question: 'URL WordPress will be installed at (ex. example.com)',
-			key: 'url',
-			required: true,
-			after: function(value) {
+			message: 'WordPress URL',
+			name: 'url',
+			default: defaults.url || null,
+			validate: requiredValidate,
+			filter: function(value) {
 				value = value.replace(/\/+$/g, '');
 				if (!/^http[s]?:\/\//.test(value)) {
 					value = 'http://' + value;
@@ -12,150 +26,165 @@ module.exports = function(advanced) {
 				return value;
 			}
 		}, {
-			question: 'WordPress Version',
-			key: 'wpVer',
-			required: true,
-			depends: advanced
+			message: 'WordPress Version',
+			name: 'wpVer',
+			default: defaults.wpVer || null,
+			validate: requiredValidate,
+			when: advancedWhen,
 		}, {
-			question: 'Table prefix',
-			key: 'tablePrefix',
-			required: true,
-			default: 'wp_'
+			message: 'Table prefix',
+			name: 'tablePrefix',
+			default: defaults.tablePrefix || 'wp_',
+			validate: requiredValidate
 		}, {
-			question: 'Database host',
-			key: 'dbHost',
-			required: true,
-			default: 'localhost'
+			message: 'Database host',
+			name: 'dbHost',
+			default: defaults.dbHost || 'localhost',
+			validate: requiredValidate
 		}, {
-			question: 'Database name',
-			key: 'dbName',
-			required: true
+			message: 'Database name',
+			name: 'dbName',
+			default: defaults.dbName || null,
+			validate: requiredValidate
 		}, {
-			question: 'Database user',
-			key: 'dbUser',
-			required: true
+			message: 'Database user',
+			name: 'dbUser',
+			default: defaults.dbUser || null,
+			validate: requiredValidate
 		}, {
-			question: 'Database password',
-			key: 'dbPass',
-			required: true
+			message: 'Database password',
+			name: 'dbPass',
+			default: defaults.dbPass || null
 		}, {
-			question: 'Use Git?',
-			key: 'git',
-			default: 'N',
-			boolean: true
+			message: 'Use Git?',
+			name: 'git',
+			default: defaults.git || 'N',
+			type: 'confirm'
 		}, {
-			question: 'Would you like to install WordPress as a submodule?',
-			key: 'submodule',
-			default: 'N',
-			boolean: true,
-			depends: function(res) {
+			message: 'Would you like to install WordPress as a submodule?',
+			name: 'submodule',
+			type: 'confirm',
+			default: defaults.submodule || false,
+			when: function(res) {
 				return !!res.git;
 			}
 		}, {
-			question: 'Would you like to install WordPress with the custom directory structure?',
-			key: 'customDirs',
-			default: 'N',
-			boolean: true,
-			depends: function(res) {
+			message: 'Would you like to install WordPress with the custom directory structure?',
+			name: 'customDirs',
+			type: 'confirm',
+			default: defaults.customDirs || false,
+			when: function(res) {
 				return !res.git || !res.submodule;
 			}
 		}, {
-			question: 'WordPress install directory',
-			key: 'wpDir',
-			default: 'wordpress',
-			depends: function(res) {
+			message: 'WordPress install directory',
+			name: 'wpDir',
+			default: defaults.wpDir || 'wordpress',
+			when: function(res) {
 				return !!res.submodule || !!res.customDirs;
 			}
 		}, {
-			question: 'WordPress content directory',
-			key: 'contentDir',
-			required: true,
-			default: 'content',
-			depends: function(res) {
+			message: 'WordPress content directory',
+			name: 'contentDir',
+			default: defaults.contentDir || 'content',
+			validate: requiredValidate,
+			when: function(res) {
 				return !!res.submodule || !!res.customDirs;
 			}
 		}, {
-			question: 'Block external WP requests?',
-			key: 'blockExternalRequests',
-			default: 'N',
-			boolean: true,
-			depends: advanced
+			message: 'Create local-config.php?',
+			name: 'createLocalConfig',
+			type: 'confirm',
+			default: defaults.createLocalConfig || false,
+			when: advancedWhen
 		}, {
-			question: 'Add wp-config.php to .gitignore?',
-			key: 'ignoreWPConfig',
-			default: 'n',
-			required: true,
-			boolean: true,
-			depends: function(res) {
+			message: 'Block external WP requests?',
+			name: 'blockExternalRequests',
+			type: 'confirm',
+			default: defaults.blockExternalRequests || false,
+			when: advancedWhen
+		}, {
+			message: 'Add wp-config.php to .gitignore?',
+			name: 'ignoreWPConfig',
+			type: 'confirm',
+			default: defaults.ignoreWPConfig || false,
+			validate: requiredValidate,
+			when: function(res) {
 				return (advanced && !!res.git);
 			}
 		}, {
-			question: 'Add WordPress Core files to .gitignore?',
-			key: 'ignoreWPCore',
-			default: 'N',
-			required: true,
-			boolean: true,
-			depends: function(res) {
-				return (advanced && !!res.git);
+			message: 'Add WordPress Core files to .gitignore?',
+			name: 'ignoreWPCore',
+			type: 'confirm',
+			default: defaults.ignoreWPCore || false,
+			validate: requiredValidate,
+			when: function(res) {
+				return (advancedWhen() && !!res.git);
 			}
 		}, {
-			question: 'Use Vagrant?',
-			key: 'vagrant',
-			required: true,
-			depends: advanced,
-			default: 'N',
-			boolean: true
+			message: 'Use Vagrant?',
+			name: 'vagrant',
+			type: 'confirm',
+			default: defaults.vagrant || false,
+			when: advancedWhen,
+			validate: requiredValidate
 		}, {
-			question: 'Install a custom theme?',
-			key: 'installTheme',
-			default: 'Y',
-			boolean: true
+			message: 'Install a custom theme?',
+			name: 'installTheme',
+			type: 'confirm',
+			default: (typeof defaults.installTheme !== 'undefined') ? defaults.installTheme : true
 		}, {
-			question: 'Destination directory (ex. twentytwelve)',
-			key: 'themeDir',
-			required: true,
-			depends: function(res) {
+			message: 'Destination directory',
+			name: 'themeDir',
+			default: defaults.themeDir || 'yeopress',
+			validate: requiredValidate,
+			when: function(res) {
 				return !!res.installTheme;
 			}
 		}, {
-			question: 'Theme source type (git/tar)',
-			key: 'themeType',
-			required: true,
-			validate: /^(?:git|tar)$/,
-			default: 'git',
-			depends: function(res) {
+			message: 'Theme source type (git/tar)',
+			name: 'themeType',
+			default: defaults.themeType || 'git',
+			validate: function(value) {
+				if (value != '' && /^(?:git|tar)$/.test(value)) {
+					return true;
+				}
+				return false;
+			},
+			when: function(res) {
 				return !!res.installTheme;
 			}
 		}, {
-			question: 'GitHub username',
-			key: 'themeUser',
-			required: true,
-			default: 'wesleytodd',
-			depends: function(res) {
+			message: 'GitHub username',
+			name: 'themeUser',
+			default: defaults.themeUser || 'wesleytodd',
+			validate: requiredValidate,
+			when: function(res) {
 				return !!res.installTheme && res.themeType == 'git';
 			}
 		}, {
-			question: 'GitHub repository name',
-			key: 'themeRepo',
-			required: true,
-			default: 'YeoPress',
-			depends: function(res) {
+			message: 'GitHub repository name',
+			name: 'themeRepo',
+			default: defaults.themeRepo || 'YeoPress',
+			validate: requiredValidate,
+			when: function(res) {
 				return !!res.installTheme && res.themeType == 'git';
 			}
 		}, {
-			question: 'Repository branch',
-			key: 'themeBranch',
-			required: true,
-			default: 'template',
-			depends: function(res) {
+			message: 'Repository branch',
+			name: 'themeBranch',
+			default: defaults.themeBranch || 'template',
+			validate: requiredValidate,
+			when: function(res) {
 				return !!res.installTheme && res.themeType == 'git';
 			}
 		}, {
-			question: 'Remote tarball url (ex. https://github.com/user/repo/tarball/master)',
-			key: 'themeTarballUrl',
-			required: true,
-			depends: function(res) {
-				return !!res.theme && res.themeType == 'tar';
+			message: 'Remote tarball url',
+			name: 'themeTarballUrl',
+			default: defaults.themeTarballUrl || 'https://github.com/wesleytodd/YeoPress/archive/template.tar.gz',
+			validate: requiredValidate,
+			when: function(res) {
+				return !!res.installTheme && res.themeType == 'tar';
 			}
 		}
 	];
